0cf2b9b255381e34e678038424d32af2e272a41d
Augusto <augusto@impa.br>
2016-07-31 17:38:05 -0300

    small changes to femto

---
+++ printser.py
@@ -0,0 +1,15 @@
+import serial
+import sys
+import pprint
+import cmd
+import time
+
+ser = serial.Serial(sys.argv[1], 9600, timeout=1)
+
+while True:
+    time.sleep(.4)
+    input = ser.readline()
+    if input != '':
+        print(input)
+
+ser.close()
